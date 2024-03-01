88ad3157c8f5e3fb5ceb36cf980c7eb34ad61391
Heseela <bhusalhishila7@gmail.com>
2024-03-01 18:57:32 +0545

    updated

--- src/HOC/Axios.jsx
+++ src/HOC/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosinstance=axios.create({
-        baseURL:"http://192.168.100.43:3000/"
+        baseURL:"http://localhost:3000/"
         // baseURL:"https://hubmainback.hubit.com.np"
 })
 
